449085509d7fa16c44a5ff16dd42370ff0f1e51a
nostcoper <carlosserrato200475@gmail.com>
2025-03-29 16:03:07 -0500

    Initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import ParticlesBg from "particles-bg";
+import Fade from "react-reveal";
+
+class Header extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const project = this.props.data.project;
+    const github = this.props.data.github;
+    const name = this.props.data.name;
+    const description = this.props.data.description;
+
+    return (
+      <header id="home">
+
+
+        <div className="row banner">
+          <div className="banner-text">
+            <Fade bottom>
+              <h1 className="responsive-headline">{name}</h1>
+            </Fade>
+            <Fade bottom duration={1200}>
+              <h3>{description}.</h3>
+            </Fade>
+            <hr />
+            <Fade bottom duration={2000}>
+              <ul className="social">
+              <a href="https://drive.usercontent.google.com/download?id=1kKwu-0MZK5K_qNPtgwU_4GKy-JWJyC58&export=download" className="button btn project-btn" download>
+                <i className="fa fa-download"></i> Descargar
+              </a>
+                <a href={github} className="button btn github-btn">
+                  <i className="fa fa-github"></i>Github
+                </a>
+              </ul>
+            </Fade>
+          </div>
+        </div>
+
+        <p className="scrolldown">
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle"></i>
+          </a>
+        </p>
+      </header>
+    );
+  }
+}
+
+export default Header;
